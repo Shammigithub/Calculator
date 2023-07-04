4e51ca0f915b66cfaa04314cc0749031307d0e15
samanritgithub <ssamankumari6@gmail.com>
2023-07-04 13:19:34 +0530

    Edit all

--- Cal.py
+++ Cal.py
@@ -1,56 +1,63 @@
+from tkinter import Tk,Entry, Button, StringVar
 
-from tkinter import *
+
+
+
+class Calculator:
+    def __init__(self, master):
+        root.title("Calculator")
+        root.geometry("570x600+100+200")
+        root.resizable(False, False)
+        root.config(bg="#17161b")
+
+        self.equation = StringVar()
+        self.entry_value = ''
+
+
+
+
+
+
+        Entry(width=17, bg='#E5FFCC', font=('Arial Bold', 28), textvariable=self.equation).place(x=0, y=0)
+
+
+
+
+        Button(root,width=11, height=4, text='(',relief='flat', bg='#CCE5FF', command=lambda: self.show('(')).place(x=0, y=50)
+        Button(width=11, height=4, text=')', relief='flat', bg='#CCE5FF', command=lambda: self.show(')')).place(x=90,  y=50)
+        Button(width=11, height=4, text='%', relief='flat', bg='#CCE5FF', command=lambda: self.show('%')).place(x=180, y=50)
+        Button(width=11, height=4, text='1', relief='flat', bg='#CCE5FF', command=lambda: self.show('1')).place(x=0, y=125)
+        Button(width=11, height=4, text='2', relief='flat', bg='#CCE5FF', command=lambda: self.show('2')).place(x=90, y=125)
+        Button(width=11, height=4, text='3', relief='flat', bg='#CCE5FF', command=lambda: self.show('3')).place(x=180, y=125)
+        Button(width=11, height=4, text='4', relief='flat', bg='#CCE5FF', command=lambda: self.show('4')).place(x=0, y=200)
+        Button(width=11, height=4, text='5', relief='flat', bg='#CCE5FF', command=lambda: self.show('5')).place(x=90, y=200)
+        Button(width=11, height=4, text='6', relief='flat', bg='#CCE5FF', command=lambda: self.show('6')).place(x=180, y=200)
+        Button(width=11, height=4, text='7', relief='flat', bg='#CCE5FF', command=lambda: self.show('7')).place(x=0, y=275)
+        Button(width=11, height=4, text='8', relief='flat', bg='#CCE5FF', command=lambda: self.show('8')).place(x=180,y=275)
+        Button(width=11, height=4, text='9', relief='flat', bg='#CCE5FF', command=lambda: self.show('9')).place(x=90, y=275)
+        Button(width=11, height=4, text='0', relief='flat', bg='#CCE5FF', command=lambda: self.show('0')).place(x=90, y=350)
+        Button(width=11, height=4, text='.', relief='flat', bg='#CCE5FF', command=lambda: self.show('.')).place(x=180, y=350)
+        Button(width=11, height=4, text='+', relief='flat', bg='#CCE5FF', command=lambda: self.show('+')).place(x=270, y=275)
+        Button(width=11, height=4, text='-', relief='flat', bg='#CCE5FF', command=lambda: self.show('-')).place(x=270, y=200)
+        Button(width=11, height=4, text='/', relief='flat', bg='#CCE5FF', command=lambda: self.show('/')).place(x=270, y=50)
+        Button(width=11, height=4, text='x', relief='flat', bg='#CCE5FF', command=lambda: self.show('x')).place(x=270, y=125)
+        Button(width=11, height=4, text='=', relief='flat', bg='#CCE5FF', command=self.solve).place(x=270, y=350)
+        Button(width=11, height=4, text='C', relief='flat', bg='#0066CC', command=self.clear).place(x=0, y=350)
+
+    def show(self, value):
+       self.entry_value += str(value)
+       self.equation.set(self.entry_value)
+
+
+    def clear(self):
+       self.entry_value = ''
+       self.equation.set(self.entry_value)
+
+
+    def solve(self):
+       result = eval(self.entry_value)
+       self.equation.set(result)
 
 root = Tk()
-root.title("Calculator")
-root.geometry("570x600+100+200")
-root.resizable(False, False)
-root.config(bg="#17161b")
-
-equation = ""
-def Show(value):
-    global equation
-    equation+=value
-    label_result.config(text=equation)
-
-def Clear():
-    equation = ""
-    label_result.config(text=equation)
-
-
-
-label_result = Label(root,width=25,height=2, text="",font=("arial 30"))
-label_result.pack()
-
-Button(root,text="C",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#3697f5", command=lambda: Clear()).place(x=10, y=100)
-Button(root,text="/",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36", command=lambda:Show("/")).place(x=150, y=100)
-Button(root,text="%",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36", command=lambda:Show("%")).place(x=290, y=100)
-Button(root,text="*",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36", command=lambda:Show("*")).place(x=430, y=100)
-
-Button(root,text="7",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36", command=lambda:Show("7")).place(x=10, y=200)
-Button(root,text="8",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36", command=lambda:Show("8")).place(x=150, y=200)
-Button(root,text="9",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36", command=lambda:Show("9")).place(x=290, y=200)
-Button(root,text="-",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36", command=lambda:Show("-")).place(x=430, y=200)
-
-Button(root,text="4",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36", command=lambda:Show("4")).place(x=10, y=300)
-Button(root,text="5",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36", command=lambda:Show("5")).place(x=150, y=300)
-Button(root,text="6",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36", command=lambda:Show("6")).place(x=290, y=300)
-Button(root,text="+",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36", command=lambda:Show("+")).place(x=430, y=300)
-
-Button(root,text="1",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36", command=lambda:Show("1")).place(x=10, y=400)
-Button(root,text="2",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36", command=lambda:Show("2")).place(x=150, y=400)
-Button(root,text="3",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36", command=lambda:Show("3")).place(x=290, y=400)
-Button(root,text="0",width=11, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36", command=lambda:Show("0")).place(x=10, y=500)
-
-Button(root,text=".",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36", command=lambda:Show(".")).place(x=290, y=500)
-Button(root,text="=",width=5, height=3,font=("arial 30 bold"), bd=1,fg="#fff",bg="#fe9037", command=lambda:Show("=")).place(x=430, y=400)
-
-
-
-
-
-
-
-
-
+Calculator = Calculator(root)
 root.mainloop()
