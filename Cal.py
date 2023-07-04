51ea8ed5082fba7b0cb8672ab073d404b5a3dd07
samanritgithub <ssamankumari6@gmail.com>
2023-07-04 12:05:29 +0530

    Show command

--- Cal.py
+++ Cal.py
@@ -7,12 +7,19 @@ root.geometry("570x600+100+200")
 root.resizable(False, False)
 root.config(bg="#17161b")
 
+equation = ""
+def Show(value):
+    global equation
+    equation+=value
+    label_result.config(text=equation)
+
+
 
 label_result = Label(root,width=25,height=2, text="",font=("arial 30"))
 label_result.pack()
 
-Button(root,text="C",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#3697f5").place(x=10, y=100)
-Button(root,text="/",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36").place(x=150, y=100)
+Button(root,text="C",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#3697f5", ).place(x=10, y=100)
+Button(root,text="/",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36", command=lambda:Show("/")).place(x=150, y=100)
 Button(root,text="%",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36").place(x=290, y=100)
 Button(root,text="*",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36").place(x=430, y=100)
 
