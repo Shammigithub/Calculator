5ff6098485f7b6af24649f0c88d72102b5308c35
samanritgithub <ssamankumari6@gmail.com>
2023-07-04 11:42:45 +0530

    create Button C

--- Cal.py
+++ Cal.py
@@ -11,6 +11,8 @@ root.config(bg="#17161b")
 label_result = Label(root,width=25,height=2, text="",font=("arial 30"))
 label_result.pack()
 
+Button(root,text="C",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#3697f5").place(x=10, y=100)
+
 
 
 
