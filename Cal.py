e1f4e97caba7ab20a5a0be2e9d26a937b777143d
samanritgithub <ssamankumari6@gmail.com>
2023-07-04 11:47:20 +0530

    create first line buttons

--- Cal.py
+++ Cal.py
@@ -12,6 +12,18 @@ label_result = Label(root,width=25,height=2, text="",font=("arial 30"))
 label_result.pack()
 
 Button(root,text="C",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#3697f5").place(x=10, y=100)
+Button(root,text="/",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36").place(x=150, y=100)
+Button(root,text="%",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36").place(x=290, y=100)
+Button(root,text="*",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36").place(x=430, y=100)
+#Button(root,text="",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36").place(x=10, y=100)
+#Button(root,text="C",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36").place(x=10, y=100)
+#Button(root,text="C",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36").place(x=10, y=100)
+#Button(root,text="C",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36").place(x=10, y=100)
+#Button(root,text="C",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36").place(x=10, y=100)
+#Button(root,text="C",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36").place(x=10, y=100)
+#Button(root,text="C",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36").place(x=10, y=100)
+#Button(root,text="C",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36").place(x=10, y=100)
+
 
 
 
