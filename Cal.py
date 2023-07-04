2e22202adede2bf87ad97956fed8ed19bc8722c6
samanritgithub <ssamankumari6@gmail.com>
2023-07-04 12:12:07 +0530

    Clear command

--- Cal.py
+++ Cal.py
@@ -13,12 +13,16 @@ def Show(value):
     equation+=value
     label_result.config(text=equation)
 
+def Clear():
+    equation = ""
+    label_result.config(text=equation)
+
 
 
 label_result = Label(root,width=25,height=2, text="",font=("arial 30"))
 label_result.pack()
 
-Button(root,text="C",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#3697f5", ).place(x=10, y=100)
+Button(root,text="C",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#3697f5", command=lambda: Clear()).place(x=10, y=100)
 Button(root,text="/",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36", command=lambda:Show("/")).place(x=150, y=100)
 Button(root,text="%",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36").place(x=290, y=100)
 Button(root,text="*",width=5, height=1,font=("arial 30 bold"), bd=1,fg="#fff",bg="#2a2d36").place(x=430, y=100)
